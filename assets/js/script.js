4d19e4f129a93e3f66fe056da447bcd5087aa293
rclank <53981655+rclank@users.noreply.github.com>
2021-11-07 04:05:01 -0500

    logic almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,38 +4,40 @@ const questions = [
         answers: ['strings', 'booleans', 'alerts', 'numbers'],
         correctAnswer: 'alerts'
     },
-    {
-        question:"The condition in an if/else statement is enclosed with _______.",
-        answers: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-        correctAnswer: 'parenthesis'
-    },
-    {
-        question:"Arrays in JavaScript can be used to store _______.",
-        answers: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-        correctAnswer: 'all of the above'
-    },
-    {
-        question:"String values must be enclosed within _______ when being assigned to variables.",
-        answers: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
-        correctAnswer: 'quotes'
-    },
-    {
-        question:"A very useful tool used during the development and debugging for printing content to the debugger is:",
-        answers: ['Javascript', 'terminal/bash', 'for loops', 'console.log'],
-        correctAnswer: 'console.log'
-    }
+    // {
+    //     question:"The condition in an if/else statement is enclosed with _______.",
+    //     answers: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
+    //     correctAnswer: 'parenthesis'
+    // },
+    // {
+    //     question:"Arrays in JavaScript can be used to store _______.",
+    //     answers: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
+    //     correctAnswer: 'all of the above'
+    // },
+    // {
+    //     question:"String values must be enclosed within _______ when being assigned to variables.",
+    //     answers: ['commas', 'curly brackets', 'quotes', 'parenthesis'],
+    //     correctAnswer: 'quotes'
+    // },
+    // {
+    //     question:"A very useful tool used during the development and debugging for printing content to the debugger is:",
+    //     answers: ['Javascript', 'terminal/bash', 'for loops', 'console.log'],
+    //     correctAnswer: 'console.log'
+    // }
 ]
 // DOM elements
 let startBtnEl = document.querySelector("#start-btn");
-let questionAreaEl = document.querySelector("#question-area");
-let quizAreaEl = document.querySelector("#quiz-area");
+let questionCardEl = document.querySelector("#question-card");
+let quizCardEl = document.querySelector("#quiz-card");
 let timerEl = document.querySelector("#timer");
-let endQuizAreaEl = document.querySelector("#end-quiz-area");
+let endQuizCardEl = document.querySelector("#end-quiz-card");
+let scoresCardEl = document.querySelector("#high-scores-card");
 
 // variables
 let questionCounter = 0;
 let timeLeft = 75;
-let penaltyTime = 10;
+const penaltyTime = 10;
+let scores;
 
 // timer functions
 let timer;
@@ -78,11 +80,25 @@ let runTimer = function() {
     // }
 }
 
+// unhide scoreCard
+let unhideScoreCard = function() {
+    // hide other elements
+    quizCardEl.classList.add('hide');
+    questionCardEl.classList.add('hide');
+    endQuizCardEl.classList.add('hide');
+    document.querySelector("header").classList.add('hide');
+
+    scoresCardEl.classList.remove("hide");
+}
+
+// view high scores link
+document.querySelector("#high-scores-link").addEventListener("click", unhideScoreCard);
+
 // capture start click
 startBtnEl.addEventListener("click", function() {
     // debugger;
-    quizAreaEl.classList.add('hide');
-    questionAreaEl.classList.remove('hide');
+    quizCardEl.classList.add('hide');
+    questionCardEl.classList.remove('hide');
 
 
     // start timer
@@ -161,9 +177,12 @@ let checkAnswer = function () {
     }
 }
 
+
+
 let endQuiz = function() {
     // stop timer, stop decrementing timer
     clearInterval(timer);
+    console.log(timerEl.textContent);
     console.log(timeLeft);
 
 
@@ -171,9 +190,9 @@ let endQuiz = function() {
     // but classList.add is smart enough to only add if class does not already exist!
     // aka, won't add another 'hide' if already there
     // i would like to know how to dynamically capture the incoming screen and hide dynamically. below is not extensible
-    quizAreaEl.classList.add('hide');
-    questionAreaEl.classList.add('hide');
-    endQuizAreaEl.classList.remove('hide');
+    quizCardEl.classList.add('hide');
+    questionCardEl.classList.add('hide');
+    endQuizCardEl.classList.remove('hide');
 
     // if (timeLeft > 0) {
     //     timerEl.textContent = timeLeft;
@@ -182,5 +201,59 @@ let endQuiz = function() {
     //     timerEl.textContent = 0;
     // }
 
-    window.alert('game over!');
+    // window.alert('game over!');
+}
+
+// go back button
+document.querySelector('#go-back-btn').addEventListener("click", function() {
+    scoresCardEl.classList.add('hide');
+    document.querySelector('header').classList.remove('hide');
+    quizCardEl.classList.remove('hide');
+})
+
+// clear high scores button
+document.getElementById('clear-scores-btn').onclick = function() {
+    localStorage.removeItem('scores');
+    document.getElementById('scoreboard').innerHTML = "";
+}
+
+// load scores
+let loadScores = function() {
+    
+}
+// update scores
+// sort scores
+
+// build scoreboard
+
+// submit button
+document.getElementById('submit-score-btn').onclick = function() {
+    let userData = {
+        initials: document.getElementById('initials').value,
+        score: timeLeft
+    }
+
+    scores = localStorage.getItem('scores');
+    if (scores === null || scores === '[]') {
+        scores = [userData];
+    } else {
+        scores = JSON.parse(scores);
+        scores.unshift(userData);
+        scores.sort((a, b) => {
+            return b.score - a.score;
+        });
+    }
+
+    localStorage.setItem('scores', JSON.stringify(scores));
+
+    // loop through up to 10 scores
+    // build score items to append to div
+    for (let i = 0; i < Math.min(scores.length, 10); i++) {
+        let scoreRecordEl = document.createElement('div');
+        scoreRecordEl.classList.add('score-record');
+        scoreRecordEl.textContent = `${i + 1}. ${scores[i].initials} - ${scores[i].score}`;
+        document.getElementById('scoreboard').appendChild(scoreRecordEl);
+    }
+
+    unhideScoreCard();
 }
